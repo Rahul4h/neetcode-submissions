c51f5d13cf8a885d34cb6227c58ce3af75261aba
Rahul Ghosh <86106000+Rahul4h@users.noreply.github.com>
2026-05-22 14:22:42 +0600

    Add: design-hashmap - submission-1

---
+++ Data Structures & Algorithms/design-hashmap/submission-1.cpp
@@ -0,0 +1,32 @@
+#include<cstring>
+class MyHashMap {
+public:
+   //vector<pair<int
+   int arr[1000001];
+    MyHashMap() {
+        memset(arr,-1,sizeof(arr));
+    }
+    
+    void put(int key, int value) {
+        arr[key]=value;
+    }
+    
+    int get(int key) {
+        return arr[key];
+        
+    }
+    
+    void remove(int key) {
+        arr[key]=-1;
+
+        
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
