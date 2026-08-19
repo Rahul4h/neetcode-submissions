df8f96a2e85f516d6c961ebab332d40261147b14
Rahul Ghosh <86106000+Rahul4h@users.noreply.github.com>
2026-08-19 20:31:46 +0600

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int countComponents(int n, vector<vector<int>>& edges) {
+    vector<bool>vis(n,false);
+    unordered_map<int,vector<int>>mp;
+    queue<int>qe;
+    for(auto& exm:edges)
+    {
+        mp[exm[0]].push_back(exm[1]);
+        mp[exm[1]].push_back(exm[0]);
+    }
+    int cnt=0;
+    for(int i=0;i<n;i++)
+    {
+       if(!vis[i])
+       {
+        qe.push(i);
+        cnt++;
+        while(!qe.empty())
+        {
+            int node=qe.front();
+            qe.pop();
+            vis[node]=true;
+            for(int child:mp[node])
+            {
+                if(!vis[child])
+                {
+                    qe.push(child);
+                }
+            }
+        }
+       }
+    }
+    return cnt;
+    
+
+    }
+};
